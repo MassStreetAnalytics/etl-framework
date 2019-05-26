7368524e052371581c853dfd0f64c59dc69d536b
Bob Wakefield <bob@MassStreet.net>
2019-05-26 17:03:15 -0500

    Added some more diagnostic tools

--- SQL Server SQL Scripts/Helper Scripts/utils.sql
+++ SQL Server SQL Scripts/Helper Scripts/utils.sql
@@ -1,6 +1,17 @@
+--View information about current users, sessions, and processes
+EXEC sp_who2
+
+--see how long it will take for
+--a rollback to complete
+KILL [SPID] WITH STATUSONLY
+--Example
+KILL 59 WITH STATUSONLY
+
 --Kill open transactions
 DBCC opentran
-KILL 64
+KILL [SPID]
+--Example
+KILL 59
 
 GO
 --Create Schema
